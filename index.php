11316f127afbfd783f335bce0edd44b83c5b4cdf
neelhtaky <Kat@kathleens-mbp.fritz.box>
2013-11-22 22:02:23 +0300

    More major changes to layout

--- index.php
+++ index.php
@@ -40,39 +40,66 @@
 		<?php while (have_posts()) : the_post(); ?>
 		<!-- Loop through posts and process each according to the code specified here  Process any code included in this region before the content of each post. -->
 
-
 			<article <?php post_class("clear entry entry_excerpt"); ?> id="post-<?php the_ID(); ?>" role="article">
-				<section class="entriescol1">
-					<?php if ( has_post_thumbnail() ) { ?>
-		        		<aside class="thumbnail">
-		        			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" ><?php the_post_thumbnail(); ?></a>
-						</aside>
-					<?php } else {}?>
 
-				    <section class="post_content">
-						<?php if ( is_home() || is_front_page() ) { ?>
-							<h2><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
-							<?php } elseif ( is_singular() || is_single() || is_404() || is_archive() ) { ?>
-								<h1><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-							<?php } else { ?>
-								<h1><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h1>
-							<?php } ?>
+
+					<?php if ( has_post_thumbnail() ) { ?>
+					<section class="post_content">
+						<div class="entriescol1">
+		        			<aside class="thumbnail">
+		        				<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>" ><?php the_post_thumbnail(); ?></a>
+							</aside>
+							<h2 class="post_title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
 							<footer class="byline meta">
 								<p>
 									<time pubdate class="published" title="<?php the_time('l, F jS, Y, g:i a'); ?>"><?php the_time('F j, Y'); ?></time>.
-				<?php if ( comments_open() ) :
-									echo '<p>';
-									comments_popup_link( 'No Responses Yet. Leave a response?','1 Response.', '% Responses.', 'comments-link', 'Sorry, Comments are closed.');
-									echo '</p>';
-							endif; ?>
-							<?php the_category(', ') ?>
-							<?php if (function_exists('the_tags')) { ?> <?php the_tags( '<p class="tags">', ', ', '.</p>' ); ?> <?php } ?>
+									<?php the_category(', ') ?>
+									<?php if ( comments_open() ) :
+										echo '<p>';
+										comments_popup_link( 'No Responses Yet. Leave a response?','1 Response.', '% Responses.', 'comments-link', 'Sorry, Comments are closed.');
+										echo '</p>';
+									endif; ?>
+
+									<?php if (function_exists('the_tags')) { ?> <?php the_tags( '<p class="tags">', ', ', '.</p>' ); ?> <?php } ?>
 								</p>
 							</footer>
-				    </section>
-				</section><!-- #entriescol1 -->
-								<p class="entriescol2"><?php echo the_excerpt(); ?></p>
+						</div><!-- #entriescol1 -->
+						<div class="entriescol2">
+
+								<h2 class="post_title_wide"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+
+								<p><?php echo the_excerpt(); ?></p>
+
+						</div><!-- entriescol2 -->
+						</section>
+
+
+					<?php } else { ?>
+					<section class="post_content">
+						<h2 class="post_title"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+						<h2 class="post_title_wide"><a href="<?php the_permalink() ?>" rel="bookmark" title="Permanent Link to <?php the_title_attribute(); ?>"><?php the_title(); ?></a></h2>
+					<footer class="byline meta">
+						<p>
+							<time pubdate class="published" title="<?php the_time('l, F jS, Y, g:i a'); ?>"><?php the_time('F j, Y'); ?></time>.
+							<?php the_category(', ') ?>
+							<?php if ( comments_open() ) :
+								echo '<p>';
+								comments_popup_link( 'No Responses Yet. Leave a response?','1 Response.', '% Responses.', 'comments-link', 'Sorry, Comments are closed.');
+								echo '</p>';
+							endif; ?>
+							<?php if (function_exists('the_tags')) { ?> <?php the_tags( '<p class="tags">', ', ', '.</p>' ); ?> <?php } ?>
+						</p>
+					</footer>
+				<p><?php echo the_excerpt(); ?></p>
+				</section>
+
+
+
+					 <?php }?>
+
+
 			</article>
+
 	<hr>
 	<?php endwhile; else: ?>
 		  <p>
