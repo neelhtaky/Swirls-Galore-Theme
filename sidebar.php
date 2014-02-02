54b976fed191174938306233ea3376163d63129c
neelhtaky <Kat@kathleens-mbp.fritz.box>
2014-02-02 18:02:51 +0300

    Added user biography in sidebar

--- sidebar.php
+++ sidebar.php
@@ -9,16 +9,24 @@
 					        <?php } else {}?>
 						        <aside class="byline meta postmetadata">
 									<p class="post_details">
-										This post was written by <address class="author vcard"><?php the_author_posts_link(); ?></address>
-										on what I think was a <?php the_time('l'); ?>, but I know it was the <?php the_time('jS'); ?> day in <?php the_time('F, Y'); ?>.
-										It was posted under <?php the_category(', ') ?>.
+										This post was written by <address class="author vcard"><?php the_author_posts_link(); ?></address>.
+										It was published on a <?php the_time('l'); ?>, which is the <?php the_time('jS'); ?> day in <?php the_time('F, Y'); ?>.
+										It was posted under the topic <?php the_category(', ') ?>.
 										It is tagged with <span class="tags"><?php the_tags('', ', ', '. '); ?></span>
 										<?php if ( comments_open() ) :
 											echo '<p>';
-											comments_popup_link( 'No Responses Yet. Leave a response?','1 Response.', '% Responses.', 'comments-link', 'Sorry, Comments are closed.');
+											comments_popup_link( 'There are no responses yet. Why not leave a response?','There is 1 response.', 'There are % responses.', 'comments-link', 'Sorry, but comments are closed.');
 											echo '</p>';
 										endif; ?>
 									</p>
+
+								<div class="postauthor">
+								<?php echo get_avatar( get_the_author_id() , 80 ); ?>
+								<p>
+									Hi, I am <?php the_author_meta( 'nickname', $author_id ); ?>. I have written <a href="<?php bloginfo('url'); ?>/?author=<?php the_author_ID(); ?>"><?php the_author_posts(); ?> articles</a> articles for <?php bloginfo('name'); ?>.</a>
+									<?php the_author_meta( 'description' ); ?>
+									</p>
+</div>
 								</aside>
 							<?php } elseif (is_category()) { ?>
 								<p>You are currently browsing the archives for the <?php single_cat_title(''); ?> category.</p>
