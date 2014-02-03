0b2a53d3e33cdc3ef270d430a55652bb198d6bdb
neelhtaky <Kat@kathleens-mbp.fritz.box>
2014-02-03 13:01:58 +0300

    Added support for WooCommerce Shopping Cart
    added support for woo commerce shopping cart. Plugin must be installed
    by user.

--- functions.php
+++ functions.php
@@ -214,5 +214,22 @@ function my_format_chat_row_id( $chat_author ) {
 	return absint( array_search( $chat_author, $_post_format_chat_ids ) ) + 1;
 }
 
+add_theme_support( 'woocommerce' );
+
 add_filter( 'use_default_gallery_style', '__return_false' );
+
+remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper', 10);
+remove_action( 'woocommerce_after_main_content', 'woocommerce_output_content_wrapper_end', 10);
+
+add_action('woocommerce_before_main_content', 'my_theme_wrapper_start', 10);
+add_action('woocommerce_after_main_content', 'my_theme_wrapper_end', 10);
+
+function my_theme_wrapper_start() {
+  echo '<section id="entries">';
+}
+
+function my_theme_wrapper_end() {
+  echo '</section>';
+}
+
 ?>
